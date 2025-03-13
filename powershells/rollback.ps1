0e46dc83ff4adad7b4e71bfc0481abfd035fa47e
KentaroKajiyama <kemushi555@icloud.com>
2025-03-13 23:14:46 +0900

    fix : delete a unnecessary script

--- powershells/rollback.ps1
+++
@@ -1,8 +0,0 @@
-$rollbackVersion = Read-Host "Enter rollback version (e.g., U1 for V1)"
-$rollbackFile = "C:/Projects/MyDatabaseProject/db_migrations/down/$rollbackVersion*.sql"
-
-if (Test-Path $rollbackFile) {
-  flyway -locations="filesystem:C:/Projects/MyDatabaseProject/db_migrations/down" migrate
-} else {
-  Write-Host "No rollback file found for version $rollbackVersion"
-}
